d32a097191fbf78bc371ca22ae1c5d7f33954123
James Schulze <James.Schulze@PlexsoftApps.com>
2022-02-27 21:07:29 -0600

    Budget app completed. 20220227

--- Scientific-Computing-Python/budget_app/budget.py
+++ Scientific-Computing-Python/budget_app/budget.py
@@ -3,22 +3,21 @@ class Category:
     self.title = title
     self.ledger = []
     self.balance = 0
+    self.total_debts = 0
 
   # deposit
   def deposit(self,amount,description=""):
-    self.ledger.append([amount,description])
+    self.ledger.append({"amount":amount,"description":description})
     self.balance += amount
-    # print("amount: {}, description: {}".format(amount,description))
-    # print(self.balance)
-    # print(self.ledger)
 
   # withdraw
   def withdraw(self,amount,description=""):
     is_enough_funds = self.check_funds(amount)
     if is_enough_funds == True:
       self.balance -= amount
+      self.total_debts += amount
       negative_amount = amount - amount * 2
-      self.ledger.append([negative_amount,description])
+      self.ledger.append({"amount":negative_amount,"description":description})
       return True
     else:
       return False
@@ -26,7 +25,7 @@ class Category:
   # get_balance
   def get_balance(self):
     return self.balance
-    
+
   # transfer
   def transfer(self,amount,other_category):
     is_enough_funds = self.check_funds(amount)
@@ -52,11 +51,12 @@ class Category:
 
     # Ledger
     for entry in self.ledger:
-      amount  = float(str(entry[0])[:7])
-      desc    = entry[1][:23]
+      print(entry["amount"])
+      amount  = float(str(entry["amount"])[:7])
+      desc    = entry["description"][:23]
 
       res += "{:<23}{:>7.2f}\n".format(desc,amount)
-
+      
     # Total
     res += "Total: {}".format(self.balance)
     # print(res)
@@ -66,4 +66,61 @@ class Category:
 
 
 def create_spend_chart(categories):
-  return "Bar chart goes here...."
+  # Copy the categories & total debts from all of the categories.
+  data = []
+  longest_title_len = 0 
+  total_debts = 0
+
+  for category in categories:
+    title = category.title
+    if len(title) > longest_title_len:
+      longest_title_len = len(title)
+    data.append(category)
+    total_debts += category.total_debts
+
+  # function to find the percentage spent for each category.
+  def get_percentage_spent(total_debts,category_debts):
+    return (category_debts / total_debts) * 100
+     
+  # Header
+  res = "Percentage spent by category\n"
+
+  # Percentage Spent Chart
+  i = 100
+  while i >= 0:
+    res += "{:>3}|".format(i)
+
+    for category in data:
+      category_debts = category.total_debts
+      percentage_spent = get_percentage_spent(total_debts,category_debts)
+      if percentage_spent >= i:
+        res += " o "
+      else:
+        res += "   "
+    
+    res += " \n"
+    i -= 10
+    
+  # dashed line
+  res += "    "
+  for category in categories:
+    res += "---"
+  res += "-\n"
+
+  # Category names  
+  row = 0
+  while row < longest_title_len:
+    res += "    "
+    for category in data: 
+      if len(category.title) < longest_title_len:
+        category.title += " "
+      cat_title = category.title[row]
+      res += " {} ".format(cat_title)
+    if not row == longest_title_len - 1:
+      res += " \n"
+    else:  
+      res += " "
+      
+    row += 1
+      
+  return res
