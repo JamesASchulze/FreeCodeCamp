e6b06a4868b2c06496e51b769aee31a83f4cfd0b
James Schulze <James.Schulze@PlexsoftApps.com>
2022-03-10 23:16:21 -0600

    Almost complete. 20220310

--- Scientific-Computing-Python/probability_calculator/prob_calculator.py
+++ Scientific-Computing-Python/probability_calculator/prob_calculator.py
@@ -1,12 +1,10 @@
 import copy
 import random
-# Consider using the modules imported above.
 
 class Hat:
     def __init__(self,**args):
         self.ball_collection = []
         for color, qty in args.items():
-            # print("Color: {}, qty: {}".format(color, qty))
             i = 0
             while i < qty:
                 self.ball_collection.append(color)
@@ -17,19 +15,51 @@ class Hat:
         print(self.contents)
 
     def draw(self,num_to_remove):
+        self.contents = copy.deepcopy(self.ball_collection)
         res = []
-
+        
         if num_to_remove > len(self.contents):
             for ball in self.contents:
                 res.append(ball)
         else:
             i = 0
-            while i < num_to_remove:
-                ball = self.contents.pop(random.randrange(0,len(self.contents)))
-                res.append(ball)
-                
-                i += 1
+        while i < num_to_remove:
+            ball = self.contents.pop(random.randrange(0,len(self.contents)))
+            res.append(ball)
+          
+            i += 1
         return res
       
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
-    pass
+    
+    matches = 0
+    
+    expected_ball_list = []
+    for color, qty in expected_balls.items():
+        i = 0
+        while i < qty:
+            expected_ball_list.append(color)
+            i += 1
+    
+    i = 0
+    while i < num_experiments:
+        draw = hat.draw(num_balls_drawn)
+        expected = copy.deepcopy(expected_ball_list)
+        print("    draw {}: {}".format(i, draw))
+        # print("expected: {}".format(expected))
+
+        for d_ball in draw:
+            for e_ball in expected:
+                if (d_ball == e_ball):
+                    expected.remove(e_ball)
+                    # print("expected: {}".format(expected))
+                    # print(e_ball)
+            
+            if (len(expected) == 0):
+                matches += 1
+                    
+        # print("Matches: {}".format(matches))
+        i += 1
+
+    probability = matches / num_experiments
+    return probability
